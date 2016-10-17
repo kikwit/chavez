d06b06e7abec2f27889b78cdd002a8931524dc9b
elondo <mbonze.elondo@gmail.com>
2016-10-17 21:38:51 +0100

    Support for signed cookies.

--- private/types.nim
+++ private/types.nim
@@ -1,4 +1,4 @@
-import asynchttpserver, asyncdispatch, cookies, httpcore, sequtils, strtabs, strutils, times
+import asynchttpserver, asyncdispatch, cookies, httpcore, md5, sequtils, strtabs, strutils, times
 import configuration
 
 const
@@ -9,14 +9,26 @@ type
     RequestHandler* = proc (context: Context): Future[void] {.closure.}
 
     Context* = ref object
-        request*: Request
-        params*: StringTableRef
         config*: Configuration
         cookies*: Cookies
+        params*: StringTableRef
+        request*: Request                
 
         responseCookies: seq[string]
 
     Cookies* = distinct StringTableRef
+
+proc getCookieSignedKey(key: string): string {.inline.} =
+    
+    result = key & ".signed"
+
+proc getCookieSignedValue(key, value, secret: string): string {.inline.} =
+    
+    result = getMD5(value & "." & key & "." & secret)
+
+proc toHttpDate(timeInfo: TimeInfo): string {.inline.} =
+    
+    result = format(timeInfo, "ddd',' dd MMM yyyy HH:mm:ss 'UTC'")
 
 proc parseCookies*(request: Request): Cookies =
 
@@ -31,19 +43,47 @@ proc parseCookies*(request: Request): Cookies =
 # proc `$`*(cookies: Cookies): string {.borrow.}
 proc hasKey*(cookies: Cookies, key: string): bool {.borrow.}
 
-proc get*(cookies: Cookies, key: string, default: string = nil): string =
+proc getCookieInternal(context: Context, key: string, default: string = nil): string =
     
-    if hasKey(cookies, key):
-        result = StringTableRef(cookies)[key]
+    if hasKey(context.cookies, key):
+        result = StringTableRef(context.cookies)[key]
     else:
+        result = default        
+
+proc getCookie*(context: Context, key: string, default: string = nil): string =
+    
+    let
+        signedKey = getCookieSignedKey(key)
+
+    if not hasKey(context.cookies, signedKey):
+        result = getCookieInternal(context, key, default)
+
+proc getSignedCookie*(context: Context, key: string, default: string = nil): string =
+    
+    let
+        value = getCookieInternal(context, key)
+        valueSigned = if not isNil(value): getCookieInternal(context, getCookieSignedKey(key)) else: nil
+ 
+    if isNil(value) or isNil(valueSigned):
         result = default
+        return
+
+    let
+        secrets = split(get(context.config, "secretKeys").str, ',')
+
+    for secret in secrets:
+        if valueSigned == getCookieSignedValue(key, value, secret):
+            result = value
+            return
+
+    result = default            
 
 proc addResponseCookies*(context: Context, headers: HttpHeaders) =
 
     for val in context.responseCookies:
         add(headers, SET_COOKIE, val)
 
-proc setCookie*(context: Context, key, value: string; domain = "", path = "", expires = "", secure = false, httpOnly = false) =
+proc setCookie*(context: Context, key, value: string; domain = "", path = "", expires = "", secure = false, httpOnly = true) =
     
     if isNil(context.responseCookies):
         context.responseCookies = @[]
@@ -53,12 +93,30 @@ proc setCookie*(context: Context, key, value: string; domain = "", path = "", ex
 
     context.responseCookies.add(cookieStr)
     
-proc setCookie*(context: Context, key, value: string, expires: TimeInfo, domain = "", path = "", noName = false, secure = false, httpOnly = false) =
+proc setCookie*(context: Context, key, value: string; expires: TimeInfo, domain = "", path = "", secure = false, httpOnly = true) =
     
     let
-        expiresStr = format(expires, "ddd',' dd MMM yyyy HH:mm:ss 'UTC'")
+        expiresStr = toHttpDate(expires)
 
     setCookie(context, key, value, domain, path, expiresStr, secure, httpOnly)
+
+proc setSignedCookie*(context: Context, key, value: string; domain = "", path = "", expires = "", secure = false, httpOnly = true) =
+    
+    let
+        secrets = get(context.config, "secretKeys").str
+        secret = split(secrets, ',')[0]
+        signedKey = getCookieSignedKey(key)
+        signedValue = getCookieSignedValue(key, value, secret)
+
+    setCookie(context, key, value, domain, path, expires, secure, httpOnly)
+    setCookie(context, signedKey, signedValue, domain, path, expires, secure, httpOnly)
+
+proc setSignedCookie*(context: Context, key, value: string, expires: TimeInfo, domain = "", path = "", secure = false, httpOnly = true) =
+    
+    let
+        expiresStr = toHttpDate(expires)
+
+    setSignedCookie(context, key, value, domain, path, expiresStr, secure, httpOnly)
 
 proc removeCookie*(context: Context, key: string) =
     
@@ -75,7 +133,7 @@ proc removeCookie*(context: Context, key: string) =
     if isNil(realKey):
         return
 
-    setCookie(context, realKey, "", expires)
+    setCookie(context, realKey, "", expires, httpOnly = false)
 
 proc clearCookies*(context: Context) =
     
